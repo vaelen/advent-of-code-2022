fe9f24eab29805bb58b1e2382ddec57d4aa3c986
Andrew C. Young <andrew@vaelen.org>
2022-12-05 12:34:40 +0900

    Added check_read_error

--- util.s
+++ util.s
@@ -39,6 +39,7 @@
     .global fputs
     .global puts
     .global int_string
+    .global check_read_error
 
 // Code Section
     
@@ -269,6 +270,28 @@ strcmp_gt:
     MOV     R0, #1              // If the first string is more, return 1
 strcmp_done:
     POP     {R2-R3,PC}
+
+check_read_error:
+    PUSH    {R0,R1,LR}
+    MOV     R1,R0
+    MOV     R0,#0
+    CMP     R1,#-4              // Check for interrupted system call
+    LDREQ   R0,=eintr
+    CMP     R1,#-5              // Check for IO error
+    LDREQ   R0,=eio   
+    CMP     R1,#-9              // Check for bad file descriptor
+    LDREQ   R0,=ebadf
+    CMP     R1,#-11             // Check for try again
+    LDREQ   R0,=efault
+    CMP     R1,#-14             // Check for bad address
+    LDREQ   R0,=eagain
+    CMP     R1,#-21             // Check for a directory
+    LDREQ   R0,=eisdir
+    CMP     R1,#-22             // Check for invalid
+    LDREQ   R0,=einval
+    CMP     R0,#0               // If we have a message to print, then print it
+    BLNE    puts         
+    POP     {R0,R1,PC}    
     
 // Data Section
     
@@ -280,3 +303,12 @@ newline_s:      .asciz "\n"
 space_s:        .asciz " "
 R_s:            .asciz "R"
 colon_s:        .asciz ": "
+
+// Error Codes
+eintr:  .asciz "[ERROR] Interrupted System Call: The call was interrupted by a signal before any data was read."
+eio:    .asciz "[ERROR] I/O Error"
+ebadf:  .asciz "[ERROR] Bad File Number: Not a valid file descriptor"
+eagain: .asciz "[ERROR] Try Again: Read would block but file is marked non-blocking"
+efault: .asciz "[ERROR] Bad Address: Buffer is outside your addressible address space"
+eisdir: .asciz "[ERROR] Trying to Read From a Directory Instead of a File"
+einval: .asciz "[ERROR] Invalid Argument: Could not read file"
